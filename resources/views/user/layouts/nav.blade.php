8a33311e8ce3ecc8e89bad45f616f15e19909f3d
Hiu Hoang <89955228+HiuHoang@users.noreply.github.com>
2022-04-17 00:42:27 +0700

    fix navbar

--- resources/views/user/layouts/nav.blade.php
+++ resources/views/user/layouts/nav.blade.php
@@ -29,6 +29,7 @@
               @else
               <li><a href="login" target="_self" class="dropdown-item">Login</a></li>
               @endif
+        </li>
         <form class="d-flex">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
@@ -37,16 +38,16 @@
   </nav>
 
   <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #e3f2fd;">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">ATN</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
