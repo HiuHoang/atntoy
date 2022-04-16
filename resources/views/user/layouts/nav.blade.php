cf9bb332facaff2a7bcb8a1088af45a75f3f1f4f
Hiu Hoang <89955228+HiuHoang@users.noreply.github.com>
2022-04-17 01:55:39 +0700

    fix 2

--- resources/views/user/layouts/nav.blade.php
+++ resources/views/user/layouts/nav.blade.php
@@ -1,16 +1,16 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #75baec;">
-  <a class="navbar-brand" href="#">ATN</a>
+  <a class="navbar-brand" href="{{ url('/') }}">ATN</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    {{-- <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
       </li>
-    </ul>
+    </ul> --}}
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
