a23a5f6a0a0fde9634f6f3baafa57eea1af6eef8
Hiu Hoang <89955228+HiuHoang@users.noreply.github.com>
2022-04-17 01:30:17 +0700

    fix 2

--- resources/views/user/layouts/nav.blade.php
+++ resources/views/user/layouts/nav.blade.php
@@ -1,39 +1,38 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #e3f2fd;">
-    <a class="navbar-brand" href="#">
-      <a class="navbar-brand" href="#">ATN</a>
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  <a class="navbar-brand" href="#">ATN</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
-        </li>
-        @if (Auth::check())
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                {{ Auth::user()->username }} <span class="caret"></span>
-            </a>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    <ul class="navbar-nav ml-auto">
+      @if (Auth::check())
+      <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+              {{ Auth::user()->username }} <span class="caret"></span>
+          </a>
 
-            <ul class="dropdown-menu" role="menu" aria-labelledby="navbarDropdown">
-                @if(Auth::user()->role==='admin')
-                <li><a href="{{ url('/manage') }}" class="dropdown-item">Dashboard</a></li>
-                @endif
-                <li><hr class="dropdown-divider"></li>
-                <li><a href="{{ url('/logout') }}" class="dropdown-item">Logout</a></li>
-            </ul>
-              @else
-              <li><a href="login" target="_self" class="dropdown-item">Login</a></li>
+          <ul class="dropdown-menu" role="menu">
+              @if(Auth::user()->role==='admin')
+              <li><a href="{{ url('/manage') }}" class="text-dark">Dashboard</a></li>
               @endif
-        </li>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-    </div>
-  </nav>
+              <li><a href="{{ url('/logout') }}" class="text-dark">Logout</a></li>
+          </ul>
+      @else
+      <li><a href="login" target="_self" class="text-dark">Login</a></li>
+              @endif
+    </ul>
+  </div>
+</nav>
 
-  <!-- Navbar -->
+<!-- Navbar -->
